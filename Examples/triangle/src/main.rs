35e46f024bfc2e8bba6dc4415f024ad1d463f21f
Hopson97 <mhopson@hotmail.co.uk>
2018-08-17 01:56:41 +0100

    fix triangle example code

--- Examples/triangle/src/main.rs
+++ Examples/triangle/src/main.rs
@@ -27,14 +27,19 @@ fn main() {
     //Load gl library
     gl::load_with(|s| window.get_proc_address(s) as *const _);
 
+    //Create a vertex array object and a vertex buffer object
     let mut vao = 0;
     let mut vbo = 0;
 
+    //Generate and bind the VAO
     gl_gen_vertex_arrays(1, &mut vao);
     gl_bind_vertex_array(vao);
 
+    //Generate and bind the VBO
     gl_gen_buffers(1, &mut vbo);
     gl_bind_buffer(GLTarget::ArrayBuffer, vbo);
+
+    //Buffer the vertex data and tell OpenGL the structure
     gl_buffer_data(GLTarget::ArrayBuffer, &VERTEX_DATA, GLUsage::StaticDraw);
     gl_enable_vertex_attrib_array(0);
     gl_vertex_attrib_pointer(0, 2, GLType::Float, false, 0);
@@ -53,11 +58,10 @@ fn main() {
             }
         });
 
+        //Draw stuff
         gl_clear_color(0.5, 0.5, 0.2, 1.0);
         unsafe {  gl::Clear(gl::COLOR_BUFFER_BIT); }
         gl_draw_arrays(GLPrimitive::Triangles, 0, 3);
-
-        gl::DrawElementsInstanced();
 
 
         window.swap_buffers().unwrap();
